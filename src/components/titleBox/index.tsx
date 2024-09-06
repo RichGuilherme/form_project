ae76b3e498791c3c5ed45787389d370aed7c1735
richGuilherme <richardbold.rg@gmail.com>
2024-09-06 09:09:41 -0300

    refator: mudei para pasta diferente os typos e interfaces

--- src/components/titleBox/index.tsx
+++ src/components/titleBox/index.tsx
@@ -1,11 +1,7 @@
 import { memo } from "react";
-
-type TitleFormProp = {
-  title: string
-}
+import { TitleFormProp } from "../type/titleForm";
 
 export const TitleBox = memo(({ title }: TitleFormProp) => {
-
   return (
     <div className="text-slate-700 text-2xl">{title}</div>
   );
